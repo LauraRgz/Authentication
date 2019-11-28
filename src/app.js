8e022f24ef0baa7f9189ff8e806b89df767a301f
LauraRgz <lrodriguezs5@alumnos.nebrija.es>
2019-11-28 11:59:21 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,73 @@
-console.log('Hello world!!!');
+import { MongoClient, ObjectID } from "mongodb";
+import { GraphQLServer } from "graphql-yoga";
+
+import "babel-polyfill";
+import { rejects } from "assert";
+import { json } from "express";
+
+const usr = "Laura";
+const pwd = "Pabl11";
+const url = "cluster0-eqbhg.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+/**
+ * Connects to MongoDB Server and returns connected client
+ * @param {string} usr MongoDB Server user
+ * @param {string} pwd MongoDB Server pwd
+ * @param {string} url MongoDB Server url
+ */
+
+const connectToDb = async function(usr, pwd, url) {
+  const uri = `mongodb+srv://${usr}:${pwd}@${url}`;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  await client.connect();
+  return client;
+};
+
+const runGraphQLServer = function(context) {
+  const typeDefs = `
+    type Bill{
+        id: ID!
+        description: String!
+        date: String!
+        user: User!
+    }
+    type User{
+        id: ID!
+        name: String!
+        password: String!
+        bills: [Bill]!
+    }
+    
+    type Query{
+        getBills(): [Bill]!
+    }
+    type Mutation{
+        addUser(): Author!
+        login(): String!
+        logout(): String!
+        removeUser(): String!
+        
+    }
+      `;
+
+  const resolvers = {
+      Query: {},
+
+      Mutation: {}
+
+
+  }
+};
+
+const runApp = async function() {
+    const client = await connectToDb(usr, pwd, url);
+    console.log("Connect to Mongo DB");
+  
+    runGraphQLServer({ client });
+  };
+  
+  runApp();
